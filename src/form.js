e0a0911647e9b060f8175732c0ae31bbd851fb2e
nithinhacks <nithinbhpv@gmail.com>
2021-10-13 19:06:53 +0530

    cors fixed

--- src/form.js
+++ src/form.js
@@ -13,7 +13,7 @@ class GfgForm extends React.Component {
     }
 
     fetchsome() {
-      const url = `https://geeks-for-geeks-api.vercel.app/${this.state.value}`;
+      const url = `/${this.state.value}`;
       axios.get(url).then((res) => {
         let data = res.data;
         if(data.error)
