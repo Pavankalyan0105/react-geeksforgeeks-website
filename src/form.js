b7e571d7f7c021daf23c89d636e7a2e86334ef92
nithinhacks <nithinbhpv@gmail.com>
2021-10-17 12:33:56 +0530

    form-responsivness, added loader

--- src/form.js
+++ src/form.js
@@ -6,7 +6,7 @@ import formimg from "./images/undraw_Online_re_x00h.png"
 class GfgForm extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {value: '', name:'', institution:'', instituteRank:'', problems:'', coding:'', error:'' };
+      this.state = {value: '', name:'', institution:'', instituteRank:'', problems:'', coding:'', error:'', loaded: true };
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
       this.fetchsome = this.fetchsome.bind(this);
@@ -18,10 +18,10 @@ class GfgForm extends React.Component {
         let data = res.data;
         if(data.error)
         {
-          this.setState({ error:'user not found' })
+          this.setState({ error:'user not found', value:'', name:"", institution:"", instituteRank:"", problems:"", coding:"", loaded: true})
         }
         else{
-          this.setState({ value:'', name:data.info.name, institution:data.info.institution, instituteRank:data.info.instituteRank, problems:data.info.solved, coding:data.info.codingScore, error:''});
+          this.setState({ value:'', name:data.info.name, institution:data.info.institution, instituteRank:data.info.instituteRank, problems:data.info.solved, coding:data.info.codingScore, error:'', loaded: true});
         }
       })
       this.state.value="";
@@ -29,6 +29,7 @@ class GfgForm extends React.Component {
   
     handleChange(event) {    this.setState({value: event.target.value});  }
     handleSubmit(event) {
+      this.setState({ loaded: false })
       this.fetchsome();
       event.preventDefault();
     }
@@ -44,19 +45,23 @@ class GfgForm extends React.Component {
             </div>
             <div className="form-main">
                 <div className="form">
-                    <form onSubmit={this.handleSubmit}>        
+                    <form onSubmit={this.handleSubmit} className="form-text" >  
+                        <div>      
                         <label>
                         GeeksforGeeks username:
-                        <input type="text" placeholder="username" value={this.state.value} onChange={this.handleChange} />        
+                        <input type="text" placeholder="username" value={this.state.value} onChange={this.handleChange} />  
                         </label>
+                        </div>
                     <input id="in" type="submit" value="Submit" />
                     </form>
-                    <p id="er">{this.state.error}</p>
-                    <p>Name: {this.state.name}</p>
-                    <p>Institution: {this.state.institution}</p>
-                    <p>Institute Rank: {this.state.instituteRank}</p>
-                    <p>Problems Solved: {this.state.problems}</p>
-                    <p>Coding Score: {this.state.coding}</p>
+                    <div className="form-display">
+                      { this.state.loaded? (<p id="er">{this.state.error}</p>) : (<div class="loader"></div>)}
+                      <p>Name: {this.state.name}</p>
+                      <p>Institution: {this.state.institution}</p>
+                      <p>Institute Rank: {this.state.instituteRank}</p>
+                      <p>Problems Solved: {this.state.problems}</p>
+                      <p>Coding Score: {this.state.coding}</p>
+                    </div>
                 </div>
             </div>
         </div>
